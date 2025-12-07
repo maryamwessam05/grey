85cc932de19c896a3aed67608a268dc14a57cc1a
maryamwessam05 <23-101015@students.eui.edu.eg>
2025-12-07 21:44:42 +0200

    footer and home page done

--- home.js
+++ home.js
@@ -94,3 +94,52 @@ for (let i = 0; i < cast.length; i++) {
         </div>
     `;
 }
+
+
+
+document.getElementById("footer").innerHTML +=
+`
+ <div class="footcontent">
+      <div class="footcol">
+          <h5>GREYS ANATOMY</h5>
+          <p>Your ultimate destination for the best deals and discounts in Egypt.</p>
+          <img src="img/footicons.svg" alt="">
+      </div>
+      <div class="footcol">
+          <h5>Quick Links</h5>
+          <div class="links">
+          <a href="">About Us</a>
+          <a href="">Stories</a>
+         
+          </div>
+      </div>
+       <div class="footcol">
+          <h5>Support</h5>
+          <div class="links">
+          <a href="">Help Center</a>
+          <a href="">Contact Us</a>
+          <a href="">Terms of Service</a>
+          <a href="">Privacy Policy</a>
+          </div>
+      </div>
+
+       <div class="footcol">
+          <h5>Contact Info</h5>
+          <div class="icontxt">
+              <img src="img/locfoot.svg" alt="">
+              <p>Nasr City, Cairo, Egypt</p>
+          </div>
+           <div class="icontxt">
+              <img src="img/call.svg" alt="">
+              <p>+20 2 2345 6789</p>
+          </div>
+           <div class="icontxt">
+              <img src="img/mai.svg" alt="">
+              <p>info@wafarly.org.eg</p>
+          </div>
+      </div>
+  </div>
+  <span>© 2024 EgyptDeals. All rights reserved. Made with Love in Egypt</span>
+
+
+`;
